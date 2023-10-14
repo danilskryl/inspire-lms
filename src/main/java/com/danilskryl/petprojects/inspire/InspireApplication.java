1ca92e471e30d7c78be4cd8b221ec6855b7431de
Danil Skryl <restillbiz@gmail.com>
2023-10-14 13:36:10 +0300

    init project

---
+++ src/main/java/com/danilskryl/petprojects/inspire/InspireApplication.java
@@ -0,0 +1,13 @@
+package com.danilskryl.petprojects.inspire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danilskryl/petprojects/inspire/InspireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilskryl.petprojects.inspire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
